4bebf1367ac1942ff9109946a94724c9269a7aa3
Hamza Hajeir <hahajeir@gmail.com>
2020-05-19 11:23:49 +0300

    Remove files

--- Advanced/F_H4_SONOFF_Basic/lib/h4plugins-0.5.4/SPIFFS/spiffs.bat
+++
@@ -1,6 +0,0 @@
-echo off
-del spiffs*.bin
-set /p v=<data/h4sv
-echo Making version %v%
-mkspiffs -c data/ spiffs_%v%_1M.bin > nul
-mkspiffs -p 256 -b 8192 -s 0x0FA000 -c data/ spiffs_%v%_4M.bin > nul
